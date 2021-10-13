27d92a439f3a18eaa3082ca69ceb758f77ffca06
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-13 01:58:25 +0100

    Initialize form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -32,6 +32,17 @@ const ContactMe = () => {
             </div>
           </div>
         </div>
+
+        <form action="" class="contact_form grid">
+          <div class="contact__inputs grid">
+            <div class="contact content">
+              <label for="" class="contact label">
+                Name
+              </label>
+              <input type="text" class="contact__input" />
+            </div>
+          </div>
+        </form>
       </div>
     </section>
   );
